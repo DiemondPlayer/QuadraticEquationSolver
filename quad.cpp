1d5318c792c4dd96960b370494646e96482200f9
DiemondPlayer <diemondplay@gmail.com>
2025-08-23 13:55:03 +0300

    added header, added input loop

--- quad.cpp
+++ quad.cpp
@@ -1,23 +1,13 @@
 #include <assert.h>
-#include <stdio.h>
 #include <math.h>
-#include <stdbool.h>
-
-#define THRESHOLD 1e-9 /* threshold below which difference between doubles are ignored and they are deemed equal */
-#define UNKNOWN_ERROR 1 /* returned by main() and printRoots() in case none of the switches work */
-#define NO_ERRORS 0 /* returned by main() and printRoots() in case the program finishes without any errors */
-#define QUADRATIC_EQUATION_INFINITE_ROOTS (-1) /* returned by quadraticEquation() in case the given equation has infinite amount of roots */
-
-void determineCoefficients(double* a, double* b, double* c);
-int printRoots(int numberOfRoots, double x1, double x2, bool isLinear);
-bool isEqualDouble(double d1, double d2, double threshold);
-int quadraticEquation(double a, double b, double c, double * x1, double * x2);
+#include <stdio.h>
+#include "quad.h"
 
 int main() {
-    double a, b, c, x1, x2;
+    double a, b, c;
+    double x1, x2;
     a = b = c = x1 = x2 = NAN;
-    // printf("%lf %lf %lf %lf %lf", a, b, c, x1, x2);
-    determineCoefficients(&a,&b,&c);
+    determineCoefficients(&a, &b, &c);
     int numberOfRoots = quadraticEquation(a, b, c, &x1, &x2);
     return printRoots(numberOfRoots, x1, x2, isEqualDouble(a, 0.0, THRESHOLD));
 }
@@ -28,14 +18,14 @@ int printRoots(int numberOfRoots, double x1, double x2, bool isLinear){
             printf("The given equation has no real roots :(");
             break;
         case 1:
-            printf("The given equation has one distinct real root:\n%lg", x1);
-            break;
-        case 2:
             if (isLinear){
                 printf("The given equation is deemed linear!\nThe given equation has only one root:\n%lg", x1);
-            }else {
-                printf("The given equation has two distinct real roots:\n%lg\n%lg", x1, x2);
+            }else{
+                printf("The given equation has one distinct real root:\n%lg", x1);
             }
+            break;
+        case 2:
+            printf("The given equation has two distinct real roots:\n%lg\n%lg", x1, x2);
             break;
         case QUADRATIC_EQUATION_INFINITE_ROOTS:
             printf("The given equation has infinite amount of real distinct roots (any x will do) :O");
@@ -47,11 +37,22 @@ int printRoots(int numberOfRoots, double x1, double x2, bool isLinear){
     return NO_ERRORS;
 }
 
+//! @brief uses getchar() until it encounters a newline char or EOF
+//! @author Leonid Yutlin
+void clearInput() {
+    int c;
+    while ((c = getchar()) != EOF && c != '\n');
+}
+
 void determineCoefficients(double* a, double* b, double* c) {
     printf("Quadratic equation solving program v1\n"
            "-------------------------------------\n");
     printf("Enter coefficients of the equation (a, b, c): ");
-    scanf("%lg %lg %lg", a, b, c);
+    while(scanf("%lg %lg %lg", a, b, c) != 3){
+        printf("ERROR! Some input was not a valid coefficient! Try again!\n");
+        printf("Enter coefficients of the equation (a, b, c): ");
+        clearInput();
+    }
     printf("The coefficients have been determined as such:\n"
            "a = %lg\n"
            "b = %lg\n"
@@ -68,7 +69,7 @@ bool isEqualDouble(double d1, double d2, double threshold) {
     return fabs(d1 - d2) < fabs(threshold);
 }
 
-//! @brief Solves quadratic equation by takin in 3 coefficients of it and putting the roots inside the pointers
+//! @brief Solves quadratic equation by takin in 3 coefficients of it and putting the roots inside the pointers (if the equation is linear the root is recorded inside x1 and x2 is assigned nan)
 //! @author Leonid Yutlin
 //! @param [in] a First non-zero coefficient
 //! @param [in] b Second coefficient
@@ -86,7 +87,8 @@ int quadraticEquation(double a, double b, double c,
     if(isEqualDouble(a, 0.0, THRESHOLD)) {
         if (isEqualDouble(b, 0.0, THRESHOLD)
         && isEqualDouble(c, 0.0, THRESHOLD)) return QUADRATIC_EQUATION_INFINITE_ROOTS;
-        *x1 = *x2 = -c/b;
+        *x1 = -c/b;
+        *x2 = NAN;
         return 1;
     }
     double discriminant = b*b - 4*a*c;
